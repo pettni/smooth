788a07a2cbedbcba30dc53fd45ef59bc81cdfdba
Petter Nilsson <pettni@umich.edu>
2021-04-17 18:20:13 -0400

    Bundle constructor from parts

--- test/test_bundle.cpp
+++ test/test_bundle.cpp
@@ -39,18 +39,38 @@ TEST(Bundle, Static)
 template<typename Scalar>
 using E3 = Eigen::Matrix<Scalar, 3, 1>;
 
-
 TEST(Bundle, Construct)
 {
   std::default_random_engine rng(5);
   using mybundle = Bundle<double, SO2, SO3, E3>;
-  mybundle b;
-  b.setIdentity();
-  b.setRandom(rng);
 
-  b.part<2>() = Eigen::Vector3d(1, 2, 3);
+  auto so2 = SO2d::Random(rng);
+  auto so3 = SO3d::Random(rng);
+  auto e3 = Eigen::Vector3d::Random().eval();
+
+  mybundle b(so2, so3, e3);
+
+  ASSERT_TRUE(b.part<0>().isApprox(so2));
+  ASSERT_TRUE(b.part<1>().isApprox(so3));
+  ASSERT_TRUE(b.part<2>().isApprox(e3));
 }
+
+template<typename Scalar>
+using SubBundle = Bundle<Scalar, SO3, E3>;
 
 TEST(Bundle, BundleOfBundle)
 {
+  std::default_random_engine rng(5);
+  using MetaBundle = Bundle<double, SO2, SubBundle, SE2>;
+
+  auto so2 = SO2d::Random(rng);
+  auto so3 = SO3d::Random(rng);
+  auto e3 = Eigen::Vector3d::Random().eval();
+  auto se2 = SE2d::Random(rng);
+
+  SubBundle<double> sb(so3, e3);
+  MetaBundle mb(std::move(so2), std::move(sb), std::move(se2));
+
+  ASSERT_TRUE(mb.part<1>().part<0>().isApprox(so3));
+  ASSERT_TRUE(mb.part<1>().part<1>().isApprox(e3));
 }
