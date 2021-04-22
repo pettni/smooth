d3f9a9e96c4e6f2df6318124e78205f5d24afa69
Petter Nilsson <pettni@umich.edu>
2021-04-22 00:00:45 -0400

    Fix bspline issue, add example

--- include/smooth/interp/bspline.hpp
+++ include/smooth/interp/bspline.hpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <ranges>
+
 #include "smooth/concepts.hpp"
 
 namespace smooth
@@ -107,20 +110,47 @@ constexpr StaticMatrix<Scalar, K + 1, K + 1> cum_card_coeffmat()
 
 
 /**
- * @brief Evaluate a cardinal bspline of order K
+ * @brief Return knot indices of the control points
  *
- * interval [beg, end] must be of size K+1
+ * Return value represents range i_{-K}, ..., i_1 where i_0 is the
+ * left point of the spline interval where t falls.
+ *
+ * Negative knot indices correspond to exterior knots, the smallest
+ * possible knot index is -K
+ *
+ * @param t time where spline is to be evaluated
+ * @param t0 start time of spline
+ * @param dt spline knot distance
+ * @return std::pair<std::size_t, std::size_t>
+ */
+template<std::size_t K>
+std::pair<int64_t, int64_t> bspline_range(double t, double t0, double dt)
+{
+  auto idx_ival = static_cast<int64_t>((t - t0) / dt);
+
+  return std::make_pair(idx_ival - K, idx_ival + 1);
+}
+
+
+/**
+ * @brief Evaluate a cardinal bspline of order K and calculate derivatives
+ *
+ *   g = g_0 * \Prod_{i=1}^{K} exp ( Btilde_i(u) * log( g_{i-1}^{-1}  * g_i ) )
+ *
+ * Where Btilde are cumulative Bspline basis functins.
  *
  * @tparam G lie group type
  * @tparam K bspline order
  * @tparam It iterator type
- * @param beg start of control points
- * @param end end of control points
- * @param u interval
+ * @param[in] ctrl_points range of control points (must be of size K + 1)
+ * @param[in] u interval location: u = (t - ti) / dt \in [0, 1)
+ * @param[out] vel calculate first order derivative w.r.t. u
+ * @param[out] acc calculate second order derivative w.r.t. u
  */
-template<LieGroupLike G, std::size_t K = 3, typename It>
-G bspline(
-  It begin, It end, typename G::Scalar u,
+template<LieGroupLike G, std::size_t K = 3, std::ranges::range Range>
+G bspline_eval(
+  Range ctrl_points,
+  typename G::Scalar u,
   std::optional<Eigen::Ref<typename G::Tangent>> vel = {},
   std::optional<Eigen::Ref<typename G::Tangent>> acc = {}
 )
@@ -128,17 +158,22 @@ G bspline(
   using Scalar = typename G::Scalar;
   Eigen::Matrix<Scalar, 1, K + 1> uvec, duvec, d2uvec;
 
-  if (end - begin != K + 1) {
-    throw std::runtime_error("bspline: wrong number of control points");
+  if (std::ranges::size(ctrl_points) != K + 1) {
+    throw std::runtime_error(
+            "bspline: control point range must be size K+1=" + std::to_string(
+              K + 1) + ", got " + std::to_string(std::ranges::size(ctrl_points)));
   }
 
   uvec(0) = Scalar(1);
+  duvec(0) = Scalar(0);
+  d2uvec(0) = Scalar(0);
+
   for (std::size_t k = 1; k != K + 1; ++k) {
     uvec(k) = u * uvec(k - 1);
     if (vel.has_value() || acc.has_value()) {
       duvec(k) = Scalar(k) * uvec(k - 1);
       if (acc.has_value()) {
-        d2uvec(k) = Scalar(k - 1) * duvec(k - 1);
+        d2uvec(k) = Scalar(k) * duvec(k - 1);
       }
     }
   }
@@ -147,9 +182,8 @@ G bspline(
   constexpr auto Ms = detail::cum_card_coeffmat<double, K>().transpose();
 
   Eigen::Matrix<Scalar, K + 1, K + 1> M =
-    Eigen::Map<const Eigen::Matrix<double, K + 1, K + 1>>(Ms[0].data()).template cast<Scalar>();
+    Eigen::Map<const Eigen::Matrix<double, K + 1, K + 1, Eigen::RowMajor>>(Ms[0].data()).template cast<Scalar>();
 
-  G g = *begin++;
   if (vel.has_value() || acc.has_value()) {
     vel.value().setZero();
     if (acc.has_value()) {
@@ -157,9 +191,11 @@ G bspline(
     }
   }
 
-  for (std::size_t j = 1; j != K + 1; ++j) {
+  G g = *std::ranges::begin(ctrl_points);
+  G gjm1 = g;  // keep track of previous g
+  for (std::size_t j = 1; const auto & gj : ctrl_points | std::views::drop(1)) {
     const Scalar Btilde = uvec.dot(M.row(j));
-    const typename G::Tangent v = ((*(begin - 1)).inverse() * *begin).log();
+    const typename G::Tangent v = (gjm1.inverse() * gj).log();
     g *= G::exp(Btilde * v);
 
     if (vel.has_value() || acc.has_value()) {
@@ -174,10 +210,106 @@ G bspline(
         acc.value() += dBtilde * G::ad(vel.value()) * v + d2Btilde * v;
       }
     }
-    ++begin;
+    gjm1 = gj;
+    ++j;
   }
 
   return g;
 }
+
+
+template<LieGroupLike G, std::size_t K = 3>
+class BSpline
+{
+public:
+  /**
+   * @brief Construct a cardinal bspline defined on [0, 1) with constant value
+   */
+  BSpline() :
+  t0_(0), dt_(1), ctrl_pts_(K+1, G::Identity())
+  {}
+
+  /**
+   * @brief Create a cardinal BSpline
+   * @param t0 start of spline
+   * @param dt end of spline
+   * @param ctrl_pts spline control points
+   *
+   * The ctrl_pts - knot points correspondence is as follows
+   *
+   * KNOT  -K  -K+1   -K+2  ...    0    1   ...  N-K
+   * CTRL   0     1      2  ...    K  K+1          N
+   *                               ^               ^
+   *                             t_min           t_max
+   *
+   * The first K ctrl_pts are exterior points and are outside
+   * the support of the spline, which means that the spline is defined on
+   * [t0_, (N-K)*dt]
+   *
+   * For interpolation purposes use an odd spline degree and set
+   *
+   *  t0 = (timestamp of first control point) + dt*K/2
+   *
+   * which aligns control points with the maximum of the corresponding
+   * basis function.
+   */
+  template<std::ranges::range R>
+  BSpline(double t0, double dt, R && ctrl_pts)
+  requires std::is_same_v<std::ranges::range_value_t<R>, G>
+  :
+  t0_(t0), dt_(dt), ctrl_pts_(std::forward<R>(ctrl_pts))
+  {}
+
+  double t_min() const
+  {
+    return t0_;
+  }
+
+  double t_max() const
+  {
+    return t0_ + (ctrl_pts_.size() - K) * dt_;
+  }
+
+  G eval(
+    double t,
+    std::optional<Eigen::Ref<typename G::Tangent>> vel = {},
+    std::optional<Eigen::Ref<typename G::Tangent>> acc = {}) const
+  {
+    // index of relevant interval
+    int64_t istar = static_cast<int64_t>((t - t0_) / dt_);
+
+    double u;
+    // clamp to end of range if necessary
+    if (istar < 0) {
+      istar = 0;
+      u = 0;
+    } else if (istar + K + 1 > ctrl_pts_.size()) {
+      istar = ctrl_pts_.size() - K - 1;
+      u = 1;
+    } else {
+      u = (t - t0_ - istar * dt_) / dt_;
+    }
+
+    G g = bspline_eval<G, K>(
+      ctrl_pts_ | std::views::drop(istar) | std::views::take(K+1),
+      u, vel, acc
+    );
+
+    if (vel.has_value()) {
+      vel.value() *= dt_;
+    }
+
+    if (acc.has_value()) {
+      acc.value() *= dt_ * dt_;
+    }
+
+    return g;
+  }
+
+private:
+  std::vector<G> ctrl_pts_;
+  double t0_, dt_;
+};
+
 
 } // namespace smooth
--- test/test_bspline.cpp
+++ test/test_bspline.cpp
@@ -1,7 +1,78 @@
 #include <gtest/gtest.h>
 
 #include "smooth/interp/bspline.hpp"
+#include "smooth/bundle.hpp"
+#include "smooth/so2.hpp"
 #include "smooth/so3.hpp"
+#include "smooth/se2.hpp"
+#include "smooth/se3.hpp"
+
+
+TEST(BSpline, Static)
+{
+  constexpr auto c3 = smooth::detail::cum_card_coeffmat<double, 3>();
+  static_assert(std::abs(c3[0][0] - 1) < 1e-8);
+  static_assert(std::abs(c3[0][1] - 5. / 6) < 1e-8);
+  static_assert(std::abs(c3[0][2] - 1. / 6) < 1e-8);
+  static_assert(std::abs(c3[0][3] - 0) < 1e-8);
+
+  static_assert(std::abs(c3[1][0] - 0) < 1e-8);
+  static_assert(std::abs(c3[1][1] - 3. / 6) < 1e-8);
+  static_assert(std::abs(c3[1][2] - 3. / 6) < 1e-8);
+  static_assert(std::abs(c3[1][3] - 0) < 1e-8);
+
+  static_assert(std::abs(c3[2][0] - 0) < 1e-8);
+  static_assert(std::abs(c3[2][1] - -3. / 6) < 1e-8);
+  static_assert(std::abs(c3[2][2] - 3. / 6) < 1e-8);
+  static_assert(std::abs(c3[2][3] - 0) < 1e-8);
+
+  static_assert(std::abs(c3[3][0] - 0) < 1e-8);
+  static_assert(std::abs(c3[3][1] - 1. / 6) < 1e-8);
+  static_assert(std::abs(c3[3][2] - -2. / 6) < 1e-8);
+  static_assert(std::abs(c3[3][3] - 1. / 6) < 1e-8);
+}
+
+
+template<smooth::LieGroupLike G>
+class BSpline : public ::testing::Test
+{};
+
+template<typename Scalar>
+using E4 = Eigen::Matrix<Scalar, 4, 1>;
+
+using GroupsToTest = ::testing::Types<
+  smooth::SO2d, smooth::SO3d, smooth::SE2d, smooth::SE3d,
+  smooth::Bundle<double, smooth::SO3, E4, smooth::SE2>
+>;
+
+TYPED_TEST_SUITE(BSpline, GroupsToTest);
+
+
+TYPED_TEST(BSpline, Constant)
+{
+  std::default_random_engine rng(5);
+
+  smooth::static_for<6>(
+    [&rng](auto k) {
+      static constexpr uint32_t K = k + 1;
+
+      std::vector<TypeParam> ctrl_pts;
+      ctrl_pts.push_back(TypeParam::Random(rng));
+      for (auto i = 0u; i != K; ++i) {
+        ctrl_pts.push_back(ctrl_pts.back());
+      }
+
+      for (double u = 0.; u < 1; u += 0.05) {
+        typename TypeParam::Tangent vel, acc;
+        auto g = smooth::bspline_eval<TypeParam, K>(ctrl_pts, u, vel, acc);
+
+        ASSERT_TRUE(g.isApprox(ctrl_pts.front()));
+        ASSERT_TRUE(vel.norm() <= 1e-8);
+        ASSERT_TRUE(acc.norm() <= 1e-8);
+      }
+    });
+}
+
 
 TEST(Smooth, BSpline)
 {
@@ -15,7 +86,7 @@ TEST(Smooth, BSpline)
 
   typename smooth::SO3d::Tangent vel, acc;
 
-  auto G = smooth::bspline<smooth::SO3d, 3>(c.begin(), c.end(), 0.5, vel, acc);
+  auto G = smooth::bspline_eval<smooth::SO3d, 3>(c, 0.5, vel, acc);
 
   std::cout << G << std::endl;
   std::cout << vel << std::endl;
