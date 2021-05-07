c3c2d2f595824788c34e495a46010652f2d9d5c5
Petter Nilsson <pettni@umich.edu>
2021-05-06 22:36:57 -0400

    WIP levenberg-marquardt impl

---
+++ examples/optim.cpp
@@ -0,0 +1,27 @@
+#include <smooth/optim/lm.hpp>
+#include <smooth/so3.hpp>
+
+
+struct MyFunctor
+{
+  using Group = smooth::SO3d;
+
+  Eigen::Vector3d operator()(const smooth::SO3d & g)
+  {
+    return g.log();
+  }
+
+  Eigen::Matrix3d df(const smooth::SO3d & g)
+  {
+    return smooth::SO3d::dr_expinv(g.log());
+  }
+};
+
+
+int main()
+{
+  MyFunctor f;
+  optimize(f);
+
+  return EXIT_SUCCESS;
+}
