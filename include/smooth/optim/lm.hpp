aa7a34358bf93fe6e9171bfa53017068c7e380f5
Petter Nilsson <pettni@umich.edu>
2021-05-19 22:48:38 -0400

    WIP qr lsq solving

--- include/smooth/optim/lm.hpp
+++ include/smooth/optim/lm.hpp
@@ -2,8 +2,106 @@
 #define SMOOTH__OPTIM__LM_HPP_
 
 #include <Eigen/Core>
+#include <Eigen/Jacobi>
 
 #include <iostream>
+
+
+
+// Solve the least-squares problem
+//
+// min_a  || [ J; diag(d) ] a + [r; 0] ||^2
+//
+template<int nx>
+Eigen::Matrix<double, nx, 1> solve_ls(
+  const Eigen::Matrix<double, nx, nx> & J,
+  const Eigen::Matrix<double, nx, 1> & d,
+  const Eigen::Matrix<double, nx, 1> & r
+) {
+  using std::sqrt;
+  // decomose Q
+
+  std::cout << "Doing QR decomp" << std::endl;
+  Eigen::ColPivHouseholderQR<Eigen::Matrix<double, nx, nx>> Jqr(J);
+
+  std::cout << "Result R" << std::endl << Jqr.matrixR();
+  std::cout << "Result P" << std::endl << Jqr.colsPermutation().toDenseMatrix() << std::endl;
+
+  // want QR decomposition of matrix
+  // [ R  ]
+  // [ P' D P ]
+  // R is upper diagonal
+  // lower part is diagonal
+
+  // need to QR decompose M using givens rotations
+  // just need to eliminate lower part
+  Eigen::Matrix<double, nx, nx> upper;
+  upper.setZero();  // TODO remove
+  Eigen::Matrix<double, nx, nx> lower;
+  upper.template triangularView<Eigen::Upper>() = Jqr.matrixR().template triangularView<Eigen::Upper>();
+
+  lower = (Jqr.colsPermutation().transpose() * d).asDiagonal();
+
+  Eigen::Matrix<double, nx, 1> QTr = Jqr.matrixQ().transpose() * r;
+
+  std::cout << "Starting elimintation" << std::endl;
+  std::cout << "upper" << std::endl << upper << std::endl;
+  std::cout << "lower" << std::endl << lower << std::endl;
+
+  Eigen::Matrix<double, 2 * nx, 2 * nx> QQ;
+  Eigen::Matrix<double, 2 * nx, nx> RR, RR0;
+  QQ.setIdentity();
+  RR.template topLeftCorner<nx, nx>() = upper;
+  RR.template bottomLeftCorner<nx, nx>() = lower;
+
+  RR0 = RR;
+
+  std::cout << RR << std::endl;
+
+  Eigen::JacobiRotation<double> rot;
+  for (auto i = 0u; i != nx; ++i)  // for each column
+  {
+    // find permuted d element
+    std::cout << "finding permuted element" << std::endl;
+    if (lower(i, i) == 0.) {
+      continue;
+    }
+
+    std::cout << "eliminating column " << i << std::endl;
+
+    // eliminate all elements in lower part
+    for (int k = i; k >= 0; --k) {  // for each row
+      rot.makeGivens(RR(i, i), RR(nx + k, i));
+
+      Eigen::Matrix<double, 2 * nx, 2 * nx> rotmat;
+      rotmat.setIdentity();
+      rotmat(i, i) = rot.c();
+      rotmat(i, nx + k) = rot.s();
+      rotmat(nx + k, i) = -rot.s();
+      rotmat(nx + k, nx + k) = rot.c();
+
+      std::cout << "k = " << k << std::endl;
+      std::cout << rotmat << std::endl;
+
+      RR = rotmat.transpose() * RR;
+      QQ = QQ * rotmat;
+    }
+
+    std::cout << "after elimination" << std::endl;
+
+    std::cout << RR << std::endl;
+  }
+
+  std::cout << "started with" << std::endl;
+  std::cout << RR0 << std::endl;
+  std::cout << "result" << std::endl;
+  std::cout << QQ * RR << std::endl;
+
+  QTr = QQ.template topLeftCorner<nx, nx>().transpose() * QTr;
+  RR.template topLeftCorner<nx, nx>().template triangularView<Eigen::Upper>().solveInPlace(QTr);
+
+  return Jqr.colsPermutation() * QTr;
+}
 
 
 template<typename _F>
@@ -40,7 +138,7 @@ void optimize(_F && f)
 
   std::cout << std::endl << std::endl;
 
-  for (int i = 0; i != 6; ++i)
+  for (int i = 0; i != 10; ++i)
   {
     std::cout << "iteration " << i << std::endl;
 
@@ -59,7 +157,10 @@ void optimize(_F && f)
 
     // solve least-squares problem
     Tangent a = lhs.colPivHouseholderQr().solve(rhs);
+    Tangent a2 = solve_ls<G::lie_dof>(J, sqrt(lambda) * diag, -r);
+
     std::cout << "step " << a.transpose() << std::endl;
+    std::cout << "step2 " << a2.transpose() << std::endl;
 
     double rpre_n = r.stableNorm();
 
