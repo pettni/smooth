a08b83507daa886054644be4c326a8485c2b5174
Petter Nilsson <pettni@umich.edu>
2021-05-20 17:35:13 -0400

    WIP lmpar algorithm, not yet perfect in lambda = 0

--- include/smooth/optim/lm.hpp
+++ include/smooth/optim/lm.hpp
@@ -3,78 +3,199 @@
 
 #include <Eigen/Core>
 #include <Eigen/Jacobi>
-
+#include <Eigen/QR>
 #include <iostream>
-
 
 /**
  * @brief Solve structured least-squares probelm
  *
- *   min_a \| [J ; diag(d)] a + [r; 0] \|^2
+ *   min_x \| [J ; diag(d)] x + [r; 0] \|^2
  *
- * Where a is a size N vector
+ * Where a is a size N vector and J is M x N with M >= N
+ * and it must hold that J^T J + D^T D is positive semi-definite
  *
- * @param J_qrdecomp QR decomposition of J with column pivoting
+ * @param J_qr QR decomposition J P = Q R of J with column pivoting
  * @param d vector of length N representing diagonal matrix
  * @param r vector with same number of elements as there are rows in J
+ * @param[out] Rt output matrix s.t. Rt' Rt = P' (J' J + D' D) P
  *
  * Currenly only supports square J matrices
  */
-template<int N>
-Eigen::Matrix<double, N, 1> solve_ls(
-  const Eigen::ColPivHouseholderQR<Eigen::Matrix<double, N, N>> & J_qrdecomp,
+template <int N, int M = N>
+requires(M >= N) Eigen::Matrix<double, N, 1> solve_ls(
+  const Eigen::ColPivHouseholderQR<Eigen::Matrix<double, M, N>> & J_qr,
   const Eigen::Matrix<double, N, 1> & d,
-  const Eigen::Matrix<double, N, 1> & r
-) {
-  Eigen::Matrix<double, N, N> upper = J_qrdecomp.matrixR();
-  Eigen::Matrix<double, N, N> lower = (J_qrdecomp.colsPermutation().transpose() * d).asDiagonal();
+  const Eigen::Matrix<double, M, 1> & r,
+  std::optional<Eigen::Ref<Eigen::Matrix<double, N, N>>> Rt = {})
+{
+  // form system
+  // [A; B] x + [a; b]
+  // where A = R
+  //       B = P' diag(D) P
+  //       a = Q' r
+  //       b = 0
+  Eigen::Matrix<double, N, N> A = J_qr.matrixR().template topLeftCorner<N, N>();
+  Eigen::Matrix<double, N, N> B = (J_qr.colsPermutation().transpose() * d).asDiagonal();
 
-  Eigen::Matrix<double, N, 1> rhs_upper, rhs_lower;
-  rhs_upper = J_qrdecomp.matrixQ().transpose() * r;
-  rhs_lower.setZero();
+  Eigen::Matrix<double, N, 1> a, b;
+  a = (J_qr.matrixQ().transpose() * r).template head<N>();
+  b.setZero();
 
-  Eigen::JacobiRotation<double> G;
-
-  // QR decomposition of A with Givens rotations;
+  // QR decomposition of [A; B] with Givens rotations;
+  // where it is known that A is A triangular and B is diagonal
+  //
   // algorithm:
   //   R = A, Q = I
-  //   for each nonzero in lower part:
+  //   for each nonzero in B part:
   //      find rotation G that eliminates nonzero without introducing new zeros
   //      Q = Q G
   //      R = G' R
+  Eigen::JacobiRotation<double> G;
   for (auto col = 0u; col != N; ++col)  // for each column
   {
     for (auto row = 0u; row != col + 1; ++row) {  // for each row above diagonal
-      G.makeGivens(upper(col, col), lower(row, col));  // eliminates lower(row, col)
+      G.makeGivens(A(col, col), B(row, col));     // eliminates B(row, col)
 
       // perform matrix multiplication R = G' R
-      upper(col, col) = G.c() * upper(col, col) - G.s() * lower(row, col);
+      A(col, col) = G.c() * A(col, col) - G.s() * B(row, col);
       for (int i = col + 1; i < N; ++i) {
-        double tmp = G.c() * upper(col, i) - G.s() * lower(row, i);
-        lower(row, i) = G.s() * upper(col, i) + G.c() * lower(row, i);
-        upper(col, i) = tmp;
+        double tmp = G.c() * A(col, i) - G.s() * B(row, i);
+        B(row, i) = G.s() * A(col, i) + G.c() * B(row, i);
+        A(col, i) = tmp;
       }
 
       // At the end we need Q matrix to multiply rhs as
-      // Q' * rhs = (I * Q0 * ... * Qk)' * rhs = Qk' * ... * Q0' * rhs
+      // Q' * rhs = (I * G0 * ... * Gk)' * rhs = Gk' * ... * G0' * rhs
       //
       // We can therefore do it as we go, here we set
-      // rhs = Q' * rhs
-      double tmp = G.s() * rhs_upper(col) + G.c() * rhs_lower(row);
-      rhs_upper(col) = G.c() * rhs_upper(col) - G.s() * rhs_lower(row);
-      rhs_lower(row) = tmp;
+      // rhs = G * rhs
+      double tmp = G.s() * a(col) + G.c() * b(row);
+      a(col) = G.c() * a(col) - G.s() * b(row);
+      b(row) = tmp;
     }
   }
 
-  // solve triangular system R z = rhs to obtain z = R^-1 z
-  upper.template triangularView<Eigen::Upper>().solveInPlace(rhs_upper);
+  if (Rt.has_value()) {
+    Rt.value().template triangularView<Eigen::Upper>()
+      = A.template triangularView<Eigen::Upper>();
+  }
+
+  // check rank of upper-diagonal A (may happen if D not full-rank)
+  int rank = 0;
+  for (auto i = 0u; i != N && A(i, i) >= Eigen::NumTraits<double>::dummy_precision(); ++i) {
+    ++rank;
+  }
+
+  // solve triangular system A z = a to obtain z = R^-1 z
+  Eigen::Matrix<double, N, 1> sol;
+  sol.tail(N - rank).setZero();
+  sol.head(rank) = A.topLeftCorner(rank, rank).template triangularView<Eigen::Upper>().solve(a.head(rank));
 
   // solution is now equal to P z
-  return -(J_qrdecomp.colsPermutation() * rhs_upper);
+  return -(J_qr.colsPermutation() * sol);
 }
 
+/**
+ * @brief Approximate a Levenberg-Marquardt parameter lambda s.t. if x solves
+ *
+ *   \| [J; sqrt(lambda) * diag(D)] x  +  [r ; 0] \|^2
+ *
+ * then either
+ *  * lambda = 0 AND \|diag(D) * x\| <= 0.1 Delta
+ *    OR
+ *  * lambda > 0 AND 0.9 Delta <= \|diag(D) * x\| <= 1.1 Delta
+ *
+ * @param J matrix MxN
+ * @param d vector size Nx1
+ * @param r vector size Mx1
+ * @param Delta scalar
+ * @param[out] x output least squares solution for final lambda
+ * */
+template <int N, int M = N>
+requires(M >= N)
+double lmpar(
+  const Eigen::Matrix<double, M, N> J,
+  const Eigen::Matrix<double, N, 1> & d,
+  const Eigen::Matrix<double, M, 1> & r,
+  double Delta,
+  std::optional<Eigen::Ref<Eigen::Matrix<double, N, 1>>> x = {}
+)
+{
+  // calculate qr decomposition
+  Eigen::ColPivHouseholderQR<Eigen::Matrix<double, M, N>> J_qr(J);
 
-template<typename _F>
+  // calculate phi(0)
+  Eigen::Matrix<double, N, 1> z_iter = J_qr.solve(-r);
+  Eigen::Matrix<double, N, N> Rt_iter = J_qr.matrixR();
+
+  Eigen::Matrix<double, N, 1> q = d.cwiseProduct(z_iter);
+  double q_norm = q.stableNorm();
+
+  double alpha = 0;
+  double phi = q_norm - Delta;
+  double dphi;
+
+  if (phi <= 0.1 * Delta) {
+    // zero solution fulfills condition
+    if (x.has_value()) {
+      x.value() = z_iter;
+    }
+    return alpha;
+  }
+
+  // lower bound
+  double l = 0;
+  if (J_qr.rank() == N) {
+    // can caluclate dphi(0) here
+    Eigen::Matrix<double, N, 1> y = J_qr.colsPermutation().inverse() * (d.cwiseProduct(q) / q_norm);
+    J_qr.matrixR().template triangularView<Eigen::Upper>().transpose().solveInPlace(y);
+
+    dphi = -q_norm * y.squaredNorm();
+    l = std::max(l, -phi / dphi);
+  }
+
+  // upper bound
+  double u = ((J * d.cwiseInverse().asDiagonal()).transpose() * r).stableNorm() / Delta;
+
+  for (auto i = 0u; i != 20; ++i) {
+    // ensure alpha stays within bounds
+    if (!(l < alpha && alpha < u)) {
+      alpha = std::max<double>(0.001 * u, sqrt(l * u));
+    }
+
+    // calculate phi
+    z_iter = solve_ls<N, M>(J_qr, sqrt(alpha) * d, r, Rt_iter);
+    Eigen::Matrix<double, N, 1> q = d.cwiseProduct(z_iter);
+    double q_norm = q.stableNorm();
+    phi = q_norm - Delta;
+
+    if (std::abs(phi) <= 0.1 * Delta) {
+      break;  // condition fulfilled
+    }
+
+    // calculate derivative of phi
+    Eigen::Matrix<double, N, 1> y = J_qr.colsPermutation().transpose() * (d.cwiseProduct(q) / q_norm);
+    Rt_iter.template triangularView<Eigen::Upper>().transpose().solveInPlace(y);
+    dphi = -q_norm * y.squaredNorm();
+
+    // update bounds
+    l = std::max<double>(l, alpha - phi / dphi);
+    if (phi < 0) {
+      u = alpha;
+    }
+
+    // update alpha
+    alpha = alpha - ((phi + Delta) / Delta) * (phi / dphi);
+  }
+
+  if (x.has_value()) {
+    x.value() = z_iter;
+  }
+
+  return alpha;
+}
+
+template <typename _F>
 void optimize(_F && f)
 {
   using F = std::decay_t<_F>;
@@ -106,8 +227,7 @@ void optimize(_F && f)
 
   std::cout << std::endl << std::endl;
 
-  for (int i = 0; i != 10; ++i)
-  {
+  for (int i = 0; i != 10; ++i) {
     std::cout << "iteration " << i << std::endl;
 
     // calculate parameter (for now 1 / trust region)
@@ -145,7 +265,8 @@ void optimize(_F && f)
     // calculate actual to predicted reduction
     double up = 1. - Eigen::numext::abs2(r.stableNorm() / rpre_n);
     double dn1 = Eigen::numext::abs2((J * a).stableNorm() / rpre_n);
-    double dn2 = 2 * Eigen::numext::abs2(std::sqrt(lambda) * (diag.cwiseProduct(a)).stableNorm() / rpre_n);
+    double dn2 =
+      2 * Eigen::numext::abs2(std::sqrt(lambda) * (diag.cwiseProduct(a)).stableNorm() / rpre_n);
     double rho = up / (dn1 + dn2);
     std::cout << "rho " << rho << std::endl;
 
@@ -156,7 +277,6 @@ void optimize(_F && f)
       Delta = 2 * diag.cwiseProduct(a).stableNorm();
     }
     std::cout << "updated Delta " << Delta << std::endl;
-
 
     // update scaling
     const auto J_n = J.colwise().stableNorm().transpose().eval();
---
+++ test/test_optim.cpp
@@ -0,0 +1,173 @@
+#include <gtest/gtest.h>
+
+#include "smooth/optim/lm.hpp"
+
+
+template<int N, int M>
+void run_leastsquares_test(bool zero_d, bool sing)
+{
+    Eigen::Matrix<double, M, N> J;
+    Eigen::Matrix<double, M, 1> r;
+    Eigen::Matrix<double, N, 1> d;
+
+    for (auto i = 0u; i != 10; ++i) {
+        J.setRandom();
+
+        if (sing) {
+            J.row(M / 2).setZero();
+        }
+
+        d.setRandom();
+        d = d + Eigen::Matrix<double, N, 1>::Ones();
+
+        if (zero_d) {
+            d.setZero();
+        }
+
+        r.setRandom();
+
+        // solve QR
+        Eigen::ColPivHouseholderQR<decltype(J)> J_qr(J);
+
+        // use our algorithm
+        auto a1 = solve_ls<N, M>(J_qr, d, r);
+
+        // solve full problem
+        Eigen::Matrix<double, N, 1> a2;
+
+        Eigen::Matrix<double, N + M, N> lhs;
+        lhs.template topLeftCorner<M, N>() = J;
+        lhs.template bottomLeftCorner<N, N>() = d.asDiagonal();
+
+        Eigen::Matrix<double, N + M, 1> rhs;
+        rhs.template head<M>() = -r;
+        rhs.template tail<N>().setZero();
+        a2 = lhs.fullPivHouseholderQr().solve(rhs);
+
+        ASSERT_TRUE(a1.isApprox(a2));
+    }
+}
+
+
+TEST(Optimization, LeastSquares)
+{
+    run_leastsquares_test<1, 1>(false, false);
+    run_leastsquares_test<5, 5>(false, false);
+    run_leastsquares_test<5, 8>(false, false);
+
+    run_leastsquares_test<1, 1>(false, true);
+    run_leastsquares_test<5, 5>(false, true);
+    run_leastsquares_test<5, 8>(false, true);
+
+    run_leastsquares_test<1, 1>(true, false);
+    run_leastsquares_test<5, 5>(true, false);
+    run_leastsquares_test<5, 8>(true, false);
+
+    run_leastsquares_test<1, 1>(true, true);
+    run_leastsquares_test<5, 5>(true, true);
+    run_leastsquares_test<5, 8>(true, true);
+}
+
+
+TEST(Optimization, LmPar)
+{
+    constexpr int M = 4, N = 4;
+    Eigen::Matrix<double, M, N> J;
+    Eigen::Matrix<double, M, 1> r;
+    Eigen::Matrix<double, N, 1> d;
+
+    double Delta = 1;
+
+    for (auto i = 0u; i != 10; ++i) {
+        J.setRandom();
+
+        d.setRandom();
+        d = d + Eigen::Matrix<double, N, 1>::Ones();
+
+        r.setRandom();
+
+        Eigen::Matrix<double, N, 1> x;
+
+        // solve QR
+        auto par = lmpar<4, 4>(J, d, r, Delta, x);
+
+        // check that x solves resulting problem
+        Eigen::ColPivHouseholderQR<decltype(J)> J_qr(J);
+        auto x_test = solve_ls<N, N>(J_qr, sqrt(par) * d, r);
+        ASSERT_TRUE(x_test.isApprox(x));
+
+        // check that parameter satisfies conditions
+        bool cond1 = (par == 0) && (d.asDiagonal() * x).norm() <= 1.1 * Delta;
+        bool cond2 = (par > 0) && std::abs((d.asDiagonal() * x).norm() - Delta) <= 0.1 * Delta;
+        ASSERT_TRUE(cond1 || cond2);
+
+    }
+}
+
+TEST(Optimization, LmParSmall) {
+    constexpr int M = 4, N = 4;
+    Eigen::Matrix<double, M, N> J;
+    Eigen::Matrix<double, M, 1> r;
+    Eigen::Matrix<double, N, 1> d;
+
+    double Delta = 0.1;
+    for (auto i = 0u; i != 10; ++i) {
+        J.setRandom();
+
+        d.setRandom();
+        d = d + Eigen::Matrix<double, N, 1>::Ones();
+
+        r.setRandom();
+
+        Eigen::Matrix<double, N, 1> x;
+
+        // solve QR
+        auto par = lmpar<4, 4>(J, d, r, Delta, x);
+
+        // check that x solves resulting problem
+        Eigen::ColPivHouseholderQR<decltype(J)> J_qr(J);
+        auto x_test = solve_ls<N, N>(J_qr, sqrt(par) * d, r);
+        ASSERT_TRUE(x_test.isApprox(x));
+
+        // check that parameter satisfies conditions
+        bool cond1 = (par == 0) && (d.asDiagonal() * x).norm() <= 1.1 * Delta;
+        bool cond2 = (par > 0) && std::abs((d.asDiagonal() * x).norm() - Delta) <= 0.1 * Delta;
+        ASSERT_TRUE(cond1 || cond2);
+
+    }
+}
+
+TEST(Optimization, LmParSing) {
+    constexpr int M = 4, N = 4;
+    Eigen::Matrix<double, M, N> J;
+    Eigen::Matrix<double, M, 1> r;
+    Eigen::Matrix<double, N, 1> d;
+
+    double Delta = 1;
+    for (auto i = 0u; i != 10; ++i) {
+        J.setRandom();
+        J.row(3).setZero();
+
+        d.setRandom();
+        d = d + Eigen::Matrix<double, N, 1>::Ones();
+
+        r.setRandom();
+
+        Eigen::Matrix<double, N, 1> x;
+
+        // solve QR
+        auto par = lmpar<4, 4>(J, d, r, Delta, x);
+
+        // check that x solves resulting problem
+        Eigen::ColPivHouseholderQR<decltype(J)> J_qr(J);
+        auto x_test = solve_ls<N, N>(J_qr, sqrt(par) * d, r);
+
+        ASSERT_TRUE(x_test.isApprox(x));
+
+        // check that parameter satisfies conditions
+        bool cond1 = (par == 0) && (d.asDiagonal() * x).norm() <= 1.1 * Delta;
+        bool cond2 = (par > 0) && std::abs((d.asDiagonal() * x).norm() - Delta) <= 0.1 * Delta;
+        ASSERT_TRUE(cond1 || cond2);
+
+    }
+}
