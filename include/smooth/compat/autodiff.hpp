1ef28c14a312f7a6733b09ba59eadd9caaeada7f
Petter Nilsson <pettni@umich.edu>
2021-05-23 12:50:25 -0400

    autodiff compatability

--- include/smooth/compat/autodiff.hpp
+++ include/smooth/compat/autodiff.hpp
@@ -1,6 +1,59 @@
 #ifndef SMOOTH__COMPAT__AUTODIFF_HPP_
 #define SMOOTH__COMPAT__AUTODIFF_HPP_
 
+// clang-format off
+#include <Eigen/Core>
+#include <autodiff/forward/forward.hpp>
+#include <autodiff/forward/eigen.hpp>
+// clang-format on
 
+#define SMOOTH_DIFF_AUTODIFF
+
+#include "smooth/common.hpp"
+#include "smooth/meta.hpp"
+
+namespace smooth::diff {
+
+/**
+ * @brief Automatic differentiation in tangent space
+ *
+ * @param f function to differentiate
+ * @param wrt... function arguments
+ * @return pair( f(wrt...), dr f_(wrt...) )
+ */
+template<typename _F, typename... _Wrt>
+auto dr_autodiff(_F && f, _Wrt &&... wrt)
+{
+  using Result   = std::invoke_result_t<_F, _Wrt...>;
+  using Scalar   = typename Result::Scalar;
+  using AdScalar = autodiff::forward::Dual<Scalar, Scalar>;
+
+  static constexpr int Nx = std::min<int>({lie_info<std::decay_t<_Wrt>>::lie_dof...}) == -1
+                            ? -1
+                            : (lie_info<std::decay_t<_Wrt>>::lie_dof + ...);
+  static constexpr int Ny = lie_info<Result>::lie_dof;
+
+  auto val = f(wrt...);
+
+  // tuple of zero-valued tangent elements
+  std::tuple<Eigen::Matrix<AdScalar, lie_info<std::decay_t<_Wrt>>::lie_dof, 1>...> a_ad(
+    Eigen::Matrix<AdScalar, lie_info<std::decay_t<_Wrt>>::lie_dof, 1>::Zero()...);
+
+  // create tuple of references to members of a_ad
+  auto a_ad_ref = std::apply(
+    std::forward_as_tuple<Eigen::Matrix<AdScalar, lie_info<std::decay_t<_Wrt>>::lie_dof, 1> &...>,
+    a_ad);
+
+  Eigen::Matrix<Scalar, Ny, Nx> jac = autodiff::forward::jacobian(
+    [&f, &val, &wrt...](auto &&... vars) -> Eigen::Matrix<AdScalar, Ny, 1> {
+      return f(wrt.template cast<AdScalar>() + vars...) - val.template cast<AdScalar>();
+    },
+    a_ad_ref,
+    a_ad_ref);
+
+  return std::make_pair(val, jac);
+}
+
+}  // namespace smooth::diff
 
 #endif  // SMOOTH__COMPAT__AUTODIFF_HPP_
--- test/test_diff.cpp
+++ test/test_diff.cpp
@@ -1,60 +1,68 @@
 #include <gtest/gtest.h>
 
 #include "smooth/diff.hpp"
-#include "smooth/so3.hpp"
+#include "smooth/se2.hpp"
+#include "smooth/se3.hpp"
 
 using namespace smooth;
 
-TEST(Differentiation, SO3)
+template<smooth::LieGroupLike G>
+class DiffTest : public ::testing::Test {
+};
+
+using GroupsToTest = testing::Types<smooth::SO2d, smooth::SE2d, smooth::SO3d, smooth::SE3d>;
+
+TYPED_TEST_SUITE(DiffTest, GroupsToTest);
+
+TYPED_TEST(DiffTest, rminus)
 {
-  smooth::SO3d g1, g2;
+  TypeParam g1, g2;
   g1.setRandom();
   g2.setRandom();
 
   auto v = g1 - g2;
 
-  auto [f1, jac1] = smooth::jacobian([&g2](auto v1) { return v1 - g2; }, g1);
-  auto [f2, jac2] = smooth::jacobian([&g1](auto v2) { return g1 - v2; }, g2);
-  auto [f3, jac3] = smooth::jacobian([](auto v1, auto v2) { return v1 - v2; }, g1, g2);
+  auto [f1, jac1] = smooth::diff::dr([&g2](auto v1) { return v1 - g2; }, g1);
+  auto [f2, jac2] = smooth::diff::dr([&g1](auto v2) { return g1 - v2; }, g2);
+  auto [f3, jac3] = smooth::diff::dr([](auto v1, auto v2) { return v1 - v2; }, g1, g2);
 
-  static_assert(decltype(jac1)::RowsAtCompileTime == 3, "Error");
-  static_assert(decltype(jac1)::ColsAtCompileTime == 3, "Error");
-  static_assert(decltype(jac2)::RowsAtCompileTime == 3, "Error");
-  static_assert(decltype(jac2)::ColsAtCompileTime == 3, "Error");
-  static_assert(decltype(jac3)::RowsAtCompileTime == 3, "Error");
-  static_assert(decltype(jac3)::ColsAtCompileTime == 6, "Error");
+  static_assert(decltype(jac1)::RowsAtCompileTime == TypeParam::lie_dof, "Error");
+  static_assert(decltype(jac1)::ColsAtCompileTime == TypeParam::lie_dof, "Error");
+  static_assert(decltype(jac2)::RowsAtCompileTime == TypeParam::lie_dof, "Error");
+  static_assert(decltype(jac2)::ColsAtCompileTime == TypeParam::lie_dof, "Error");
+  static_assert(decltype(jac3)::RowsAtCompileTime == TypeParam::lie_dof, "Error");
+  static_assert(decltype(jac3)::ColsAtCompileTime == 2 * TypeParam::lie_dof, "Error");
 
-  auto jac1_true = smooth::SO3d::dr_expinv(v);
-  auto jac2_true = -smooth::SO3d::dl_expinv(v);
+  auto jac1_true = TypeParam::dr_expinv(v);
+  auto jac2_true = -TypeParam::dl_expinv(v);
 
   ASSERT_TRUE(f1.isApprox(f2));
   ASSERT_TRUE(f1.isApprox(v));
 
   ASSERT_TRUE(jac1.isApprox(jac1_true, 1e-5));
   ASSERT_TRUE(jac2.isApprox(jac2_true, 1e-5));
-  ASSERT_TRUE(jac1.isApprox(jac3.leftCols<3>(), 1e-5));
-  ASSERT_TRUE(jac2.isApprox(jac3.rightCols<3>(), 1e-5));
+  ASSERT_TRUE(jac1.isApprox(jac3.template leftCols<TypeParam::lie_dof>(), 1e-5));
+  ASSERT_TRUE(jac2.isApprox(jac3.template rightCols<TypeParam::lie_dof>(), 1e-5));
 }
 
-TEST(Differentiation, GroupToGroup)
+TYPED_TEST(DiffTest, composition)
 {
-  smooth::SO3d g1, g2;
+  TypeParam g1, g2;
   g1.setRandom();
   g2.setRandom();
 
-  auto [f1, jac1] =
-    smooth::jacobian([](auto v1, auto v2) -> smooth::SO3d { return v1 * v2; }, g1, g2);
+  auto [f1, jac1] = smooth::diff::dr([](auto v1, auto v2) -> TypeParam { return v1 * v2; }, g1, g2);
 
-  ASSERT_EQ(jac1.cols(), 6);
-  ASSERT_EQ(jac1.rows(), 3);
+  ASSERT_EQ(jac1.cols(), 2 * TypeParam::lie_dof);
+  ASSERT_EQ(jac1.rows(), TypeParam::lie_dof);
 
   auto jac1_true = g2.inverse().Ad();
-  auto jac2_true = Eigen::Matrix3d::Identity();
+  auto jac2_true = decltype(jac1_true)::Identity();
 
   ASSERT_TRUE(f1.isApprox(g1 * g2, 1e-5));
 
-  ASSERT_TRUE(jac1.leftCols(3).isApprox(jac1_true, 1e-5));
-  ASSERT_TRUE(jac1.rightCols(3).isApprox(jac2_true, 1e-5));
+  ASSERT_TRUE(jac1.template leftCols<TypeParam::lie_dof>().isApprox(jac1_true, 1e-5));
+  ASSERT_TRUE(jac1.template rightCols<TypeParam::lie_dof>().isApprox(jac2_true, 1e-5));
 }
 
 TEST(Differentiation, Dynamic)
@@ -62,7 +70,7 @@ TEST(Differentiation, Dynamic)
   Eigen::VectorXd v(3);
   v.setRandom();
 
-  auto [f1, jac1] = smooth::jacobian([](auto v1) { return (2 * v1).eval(); }, v);
+  auto [f1, jac1] = smooth::diff::dr([](auto v1) { return (2 * v1).eval(); }, v);
 
   static_assert(decltype(jac1)::RowsAtCompileTime == -1, "Error");
   static_assert(decltype(jac1)::ColsAtCompileTime == -1, "Error");
@@ -80,7 +88,7 @@ TEST(Differentiation, Mixed)
   Eigen::Vector3d v(3);
   v.setRandom();
 
-  auto [f1, jac1] = smooth::jacobian(
+  auto [f1, jac1] = smooth::diff::dr(
     [](auto v1) {
       Eigen::VectorXd ret(2);
       ret << 2. * v1(1), 2. * v1(0);
