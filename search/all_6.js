f49e35d3357180f77dbca4d010fa24717d926e44
pettni <pettni@users.noreply.github.com>
2021-08-03 16:14:00 +0000

    deploy: bc09a2c8b2f0aed34be45deca87c8a8bc8fd49d0

--- search/all_6.js
+++ search/all_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['fit_5fbspline_43',['fit_bspline',['../bspline_8hpp.html#a60e5620d092c1e814ec922ab9017950a',1,'smooth']]],
-  ['fit_5fcubic_5fbezier_44',['fit_cubic_bezier',['../bezier_8hpp.html#ac23cd0ab2555698055581e4d09b4bb95',1,'smooth']]],
+  ['fit_5fcubic_5fbezier_44',['fit_cubic_bezier',['../bezier_8hpp.html#a3b7f0ccc4f875c6d4a231e364e8d96b4',1,'smooth']]],
   ['fit_5flinear_5fbezier_45',['fit_linear_bezier',['../bezier_8hpp.html#a41fa4ef69fa4622f8c8d44397934d459',1,'smooth']]],
   ['fit_5fquadratic_5fbezier_46',['fit_quadratic_bezier',['../bezier_8hpp.html#a99b8315f72d2e3690647a718fa6f85cd',1,'smooth']]],
   ['ftol_47',['ftol',['../structsmooth_1_1MinimizeOptions.html#a477169427f1ef9f4dcc83638c250fc4b',1,'smooth::MinimizeOptions']]]
