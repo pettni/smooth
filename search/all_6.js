5e718fd67f9f90f178251ea7ac65ae82f9209507
pettni <pettni@users.noreply.github.com>
2021-12-14 16:18:05 +0000

    deploy: bd6419cda92e9e185696a52546bf932f994245d4

--- search/all_6.js
+++ search/all_6.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['fit_2ehpp_67',['fit.hpp',['../fit_8hpp.html',1,'']]],
-  ['fit_5fbspline_68',['fit_bspline',['../fit_8hpp.html#ac6ca39811c774d7d3614f4bcccf542a9',1,'smooth']]],
-  ['fit_5fspline_69',['fit_spline',['../fit_8hpp.html#a0b2ddf72f5eaa39f72ae77af28d925e5',1,'smooth']]],
-  ['fit_5fspline_5f1d_70',['fit_spline_1d',['../fit_8hpp.html#aa2bc4ae01bb329d9e53da1f3ac339c28',1,'smooth']]],
-  ['fit_5fspline_5fcubic_71',['fit_spline_cubic',['../fit_8hpp.html#aa4855541214c77b0a5626176c55308fc',1,'smooth']]],
-  ['fixeddercubic_72',['FixedDerCubic',['../structsmooth_1_1spline__specs_1_1FixedDerCubic.html',1,'smooth::spline_specs']]],
-  ['ftol_73',['ftol',['../structsmooth_1_1MinimizeOptions.html#a477169427f1ef9f4dcc83638c250fc4b',1,'smooth::MinimizeOptions']]]
+  ['fit_2ehpp_68',['fit.hpp',['../fit_8hpp.html',1,'']]],
+  ['fit_5fbspline_69',['fit_bspline',['../fit_8hpp.html#ac6ca39811c774d7d3614f4bcccf542a9',1,'smooth']]],
+  ['fit_5fspline_70',['fit_spline',['../fit_8hpp.html#a0b2ddf72f5eaa39f72ae77af28d925e5',1,'smooth']]],
+  ['fit_5fspline_5f1d_71',['fit_spline_1d',['../fit_8hpp.html#aa2bc4ae01bb329d9e53da1f3ac339c28',1,'smooth']]],
+  ['fit_5fspline_5fcubic_72',['fit_spline_cubic',['../fit_8hpp.html#aa4855541214c77b0a5626176c55308fc',1,'smooth']]],
+  ['fixeddercubic_73',['FixedDerCubic',['../structsmooth_1_1spline__specs_1_1FixedDerCubic.html',1,'smooth::spline_specs']]],
+  ['ftol_74',['ftol',['../structsmooth_1_1MinimizeOptions.html#a477169427f1ef9f4dcc83638c250fc4b',1,'smooth::MinimizeOptions']]]
 ];
--- search/all_c.js
+++ search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['nativeliegroup_151',['NativeLieGroup',['../lie__group_8hpp.html#a08f7b83e6e2f98323382899350bacbd5',1,'smooth']]],
-  ['noconstraints_152',['NoConstraints',['../structsmooth_1_1spline__specs_1_1NoConstraints.html',1,'smooth::spline_specs']]],
-  ['numerical_153',['Numerical',['../diff_8hpp.html#ad993f4e6edc070efeac1192d6663c7c2a5225763efc97aebd39e5a303fceb57ac',1,'smooth::diff']]]
+  ['nativeliegroup_152',['NativeLieGroup',['../lie__group_8hpp.html#a08f7b83e6e2f98323382899350bacbd5',1,'smooth']]],
+  ['noconstraints_153',['NoConstraints',['../structsmooth_1_1spline__specs_1_1NoConstraints.html',1,'smooth::spline_specs']]],
+  ['numerical_154',['Numerical',['../diff_8hpp.html#ad993f4e6edc070efeac1192d6663c7c2a5225763efc97aebd39e5a303fceb57ac',1,'smooth::diff']]]
 ];
