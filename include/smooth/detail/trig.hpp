9768abb09e12c48e6a57e096df09b534de340330
Petter Nilsson <pettni@umich.edu>
2022-05-22 19:44:06 -0400

    Re-enable tests

--- include/smooth/detail/trig.hpp
+++ include/smooth/detail/trig.hpp
@@ -2,23 +2,18 @@
 
 #pragma once
 
+/**
+ * @file
+ * @brief Taylor tails of sin and cos of varying degrees.
+ *
+ * The functions take in a squared argument to play nicer with autodiff.
+ */
+
 #include <cmath>
 
 #include "common.hpp"
 
 namespace smooth::detail {
-
-template<typename S>
-S sin_1(const S & x2)
-{
-  using std::sin, std::sqrt;
-  if (x2 > S(eps2)) {
-    const S x = sqrt(x2);
-    return sin(x) / x;
-  } else {
-    return S(1) - x2 / S(6) + x2 * x2 / S(120);
-  }
-}
 
 template<typename S>
 S cos_2(const S & x2)
