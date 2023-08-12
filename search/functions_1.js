b83718d09775d6ebf69da01c2f67ba1c659b9f79
pettni <pettni@users.noreply.github.com>
2023-08-12 19:30:22 +0000

    deploy: b66061585b59ad9c6026c6a1a2fd4f3e56c0d292

--- search/functions_1.js
+++ search/functions_1.js
@@ -4,6 +4,6 @@ var searchData=
   ['block_1',['block',['../structsmooth_1_1StaticMatrix.html#a5838c94ccd317906030902c26eff0429',1,'smooth::StaticMatrix']]],
   ['bspline_2',['BSpline',['../classsmooth_1_1BSpline.html#a632441d31380304827cc18696f187acf',1,'smooth::BSpline::BSpline()'],['../classsmooth_1_1BSpline.html#a0d6acf53896a7500cd446d0bebe4a4cf',1,'smooth::BSpline::BSpline(double t0, double dt, std::vector&lt; G &gt; &amp;&amp;ctrl_pts)'],['../classsmooth_1_1BSpline.html#a46819513ef5653cb07456bd5d2ef05bd',1,'smooth::BSpline::BSpline(double t0, double dt, const Rv &amp;ctrl_pts)'],['../classsmooth_1_1BSpline.html#a750d51175fecdcdd4b6da8c58a3c8835',1,'smooth::BSpline::BSpline(const BSpline &amp;)=default'],['../classsmooth_1_1BSpline.html#a5bdd526e41de2f581ef352af8a3bad7c',1,'smooth::BSpline::BSpline(BSpline &amp;&amp;)=default']]],
   ['bspline_5fbasis_3',['bspline_basis',['../basis_8hpp.html#a1843fd15f91787b986824c50312e2e7b',1,'smooth::detail']]],
-  ['bundle_4',['Bundle',['../classsmooth_1_1Bundle.html#a44d2691be06c9d31494b1f866f9c7c30',1,'smooth::Bundle']]],
+  ['bundle_4',['Bundle',['../classsmooth_1_1Bundle.html#a06dac8b73b8eb3d4026e4f191630804b',1,'smooth::Bundle']]],
   ['so3_5',['so3',['../classsmooth_1_1SE__K__3.html#ae0a22123b89e7fde4cced06f44f923a6',1,'smooth::SE_K_3']]]
 ];
